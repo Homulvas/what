8755c9bdf95ddf7c0d8e865265a8744125358347
Ignas Kaziukenas <the.qwer@gmail.com>
2011-10-03 22:21:19 +0100

    First commit

---
+++ what/settings.py
@@ -0,0 +1,16 @@
+# Scrapy settings for what project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'what'
+BOT_VERSION = '1.0'
+
+SPIDER_MODULES = ['what.spiders']
+NEWSPIDER_MODULE = 'what.spiders'
+DEFAULT_ITEM_CLASS = 'what.items.WhatItem'
+USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
