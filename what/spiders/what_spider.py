2fb159e8ae4ee17515261b6048ccfa495512fc84
Ignas Kaziukenas <the.qwer@gmail.com>
2011-10-04 21:36:45 +0100

    Adding login

--- what/spiders/what_spider.py
+++ what/spiders/what_spider.py
@@ -1,4 +1,6 @@
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
 
 class WhatSpider(BaseSpider):
     name = "what.cd"
@@ -8,5 +10,21 @@ class WhatSpider(BaseSpider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+        name = raw_input("name> ")
+        passw = raw_input("pass> ")
+        return [FormRequest.from_response(response, formdata={'username': name, 'password': passw}, callback=self.after_login)]
+                    
+    def after_login(self, response):
+        # check login succeed before going on
+        if "authentication failed" in response.body:
+            self.log("Login failed", level=log.ERROR)
+            return
+        # We've successfully authenticated, let's have some fun!
+        else:
+            return Request(url="http://www.example.com/tastypage/", callback=self.parse_tastypage)
+    
+    def parse_tastypage(self, response):
+        hxs = HtmlXPathSelector(response)
+        yum = hxs.select('//img')
+    
+        # etc.
