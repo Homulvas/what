9afaa228f28af9d9f7c61d7e0b2ec0b8bcb507a9
Ignas Kaziukenas <the.qwer@gmail.com>
2011-12-20 22:34:58 +0200

    Some changes

--- what/pipelines.py
+++ what/pipelines.py
@@ -5,19 +5,22 @@
 
 import sqlite3
 
-connection = sqlite3.connect('db')
-cursor = connection.cursor()
-cursor.execute('''create table if not exists albums (artist text, album text, year text)''')
 new = []
 
 class WhatPipeline(object):
 
+    connection = 0
+    cursor = 0
+
     def __init__(self, download_func=None):
+        global connection
+        global cursor
         connection = sqlite3.connect('db')
         cursor = connection.cursor()
         cursor.execute('''create table if not exists albums (artist text, album text, year text)''')
 
     def process_item(self, item, spider):
+        global cursor
         cursor.execute('''select rowid from albums where artist = ? and album = ? and year = ?''', (item['group'], item['album'], item['year']))
         data=cursor.fetchall()
         if len(data)==0:
@@ -26,6 +29,7 @@ class WhatPipeline(object):
         return item
 
     def close_spider(self, spider):
+        global connection
         connection.commit()
         for item in new:
             print item
